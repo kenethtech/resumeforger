d0886ddef4d9f427e335c0dc5aacacf1f2fd4fa8
kenethtech <77956449+kenethtech@users.noreply.github.com>
2026-06-29 13:42:17 +0100

    Updated test_home_page to assert status code 302 for redirecting

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,7 +2,7 @@
 
 def test_home_page(client):
     response = client.get('/')
-    assert response.status_code == 200
+    assert response.status_code == 302
 
 def test_login_page(client):
     response = client.get('/login')
